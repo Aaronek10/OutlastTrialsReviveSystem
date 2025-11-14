6d1d647c210f7894d87e76176ff781ae02456029
CraftPig <crftpig@gmail.com>
2025-11-14 15:04:55 +0100

    audio organization
    audio enhancements

--- lua/autorun/outlast_loader.lua
+++ lua/autorun/outlast_loader.lua
@@ -1,3 +1,10 @@
+--[[
+-----------------------------------------------------------------------------------------------------
+Code
+-----------------------------------------------------------------------------------------------------
+]]
+
+
 if SERVER then
     local function LoadFiles(folder)
         local files, _ = file.Find(folder .. "/*.lua", "LUA")
@@ -41,3 +48,137 @@ else
         print("[Outlast Trials] Client files reloaded.")
     end)
 end
+
+--[[
+-----------------------------------------------------------------------------------------------------
+Sound Tables
+-----------------------------------------------------------------------------------------------------
+]]
+
+-- Murder
+sound.Add( {
+    name = "OutlastTrials.MurderIntroKick",
+    channel = CHAN_WEPAON,
+    volume = 1,
+    level = 75,
+    pitch = {95, 110},
+    sound = {
+        "outlasttrials/murder/SFX_Murder_IntroKick.ogg",
+    }
+} )
+
+-- Foley
+sound.Add( {
+    name = "OutlastTrials.TorsoHeavyLong",
+    channel = CHAN_BODY,
+    volume = 0.5,
+    level = 70,
+    pitch = {95, 110},
+    sound = {
+        "outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Long01.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Long02.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Long03.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Long04.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Long05.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Long06.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Long07.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Long08.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Long09.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Long10.ogg",
+    }
+} )
+sound.Add( {
+    name = "OutlastTrials.TorsoHeavyShort",
+    channel = CHAN_BODY,
+    volume = 1,
+    level = 70,
+    pitch = {95, 110},
+    sound = {
+        "outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short01.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short02.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short03.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short04.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short05.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short06.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short07.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short08.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short09.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short10.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short11.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short12.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short13.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short14.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short15.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short16.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short17.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonHeavy_Short18.ogg",
+    }
+} )
+sound.Add( {
+    name = "OutlastTrials.TorsoLightLong",
+    channel = CHAN_BODY,
+    volume = 1,
+    level = 70,
+    pitch = {95, 110},
+    sound = {
+        "outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long01.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long02.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long03.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long04.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long05.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long06.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long07.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long08.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long09.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long10.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long11.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long12.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long13.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long14.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long15.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long16.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Long17.ogg",
+    }
+} )
+sound.Add( {
+    name = "OutlastTrials.TorsoLightShort",
+    channel = CHAN_BODY,
+    volume = 1,
+    level = 60,
+    pitch = {95, 110},
+    sound = {
+        "outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short01.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short02.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short03.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short04.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short05.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short06.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short07.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short08.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short09.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short10.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short11.ogg",
+		"outlasttrials/foley/cotton/torso/FOL_Player_Torso_CottonLight_Short12.ogg",
+    }
+} )
+
+-- Impact
+sound.Add( {
+    name = "OutlastTrials.PVPStab",
+    channel = CHAN_WEPAON,
+    volume = 1,
+    level = 75,
+    pitch = {95, 110},
+    sound = {
+        "outlasttrials/impact/SFX_Player_PVP_Knife_Stab01.ogg",
+		"outlasttrials/impact/SFX_Player_PVP_Knife_Stab02.ogg",
+		"outlasttrials/impact/SFX_Player_PVP_Knife_Stab03.ogg",
+		"outlasttrials/impact/SFX_Player_PVP_Knife_Stab04.ogg",
+		"outlasttrials/impact/SFX_Player_PVP_Knife_Stab05.ogg",
+		"outlasttrials/impact/SFX_Player_PVP_Knife_Stab06.ogg",
+    }
+} )
+
+
+
+
